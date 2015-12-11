903cd94d63177b75bb6d8e5d1ac427d09383caf4
Luis López <ldotlopez@gmail.com>
2015-12-11 16:56:27 +0100

    pep8 changes

--- ldotcommons/logging.py
+++ ldotcommons/logging.py
@@ -1,20 +1,21 @@
-#coding: utf-8
+# -*- coding: utf-8 -*-
 
-#Copyright (C) 2012 Jacobo Tarragón
+# Copyright (C) 2012 Jacobo Tarragón
 #
-#This program is free software; you can redistribute it and/or
-#modify it under the terms of the GNU General Public License
-#as published by the Free Software Foundation; either version 2
-#of the License, or (at your option) any later version.
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
 #
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-#You should have received a copy of the GNU General Public License
-#along with this program; if not, write to the Free Software
-#Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+# USA.
 
 import functools
 import logging
@@ -61,7 +62,7 @@ def get_logger(key=None):
     if key is None:
         key = utils.prog_name()
 
-    if not key in _loggers:
+    if key not in _loggers:
         _loggers[key] = logging.getLogger(key)
         _loggers[key].setLevel(_log_level)
 
