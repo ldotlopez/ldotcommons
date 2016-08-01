8dfb07910475a58b7930960d670c03534bb0a871
Luis López <ldotlopez@gmail.com>
2016-08-01 21:04:51 +0200

    Rename ldotcommons to appkit

--- setup.py
+++ setup.py
@@ -3,20 +3,20 @@
 from distutils.core import setup
 
 setup(
-    name='ldotcommons',
+    name='appkit',
     version='0.0.0.20160520.1',
     author='Luis López',
     author_email='ldotlopez@gmail.com',
-    packages=['ldotcommons'],
+    packages=['appkit'],
     scripts=[],
-    url='https://github.com/ldotlopez/ldotcommons',
+    url='https://github.com/ldotlopez/appkit',
     license='LICENSE.txt',
     description='Useful ldotlopez\'s stuff',
     long_description=open('README').read(),
     install_requires=[
-        "aiohttp",
-        "appdirs",
-        "pyyaml",
-        "sqlalchemy",
+        # "aiohttp",
+        # "appdirs",
+        # "pyyaml",
+        # "sqlalchemy",
     ],
 )
